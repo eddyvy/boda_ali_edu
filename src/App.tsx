6b34399d36277881d87ab981f1962bf77e1ccf1d
Eddy <edwardyue93@gmail.com>
2022-12-06 16:08:40 +0100

    Estructura inicial del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material'
+
+function App() {
+  return (
+    <Box component="main" sx={{ display: 'flex', flexDirection: 'column' }}>
+      <h1>Hola</h1>
+    </Box>
+  )
+}
+
+export default App
